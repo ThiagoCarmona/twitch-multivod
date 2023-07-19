1ef4b1dbf4b5326e81708b911f229e202563866a
Thiago Carmona <Thggcarmona@gmail.com>
2023-07-19 08:29:12 -0300

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
-import https from 'https';
-
-const agent = new https.Agent({
-  rejectUnauthorized: false
-});
 
 const api = axios.create({
   baseURL: "https://thiagocarmona.xyz",
-  httpsAgent: agent
 });
 
 export const getSyncVods = async (vodId: string, channels: string[], minutes?: number) => {
