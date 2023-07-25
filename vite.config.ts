219cd333f30e04ef31f6107d3675ee9c5fae9b27
Thiago Carmona <Thggcarmona@gmail.com>
2023-07-25 15:06:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/twitch-multivod'
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv("mock", process.cwd(), "");
+  
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL || "/",
+  };
+});
